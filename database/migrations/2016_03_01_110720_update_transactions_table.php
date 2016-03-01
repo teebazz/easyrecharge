782164955d38e9a019b66cff28dcdee39b97ca91
Badejo Oluwatobi <teebazzy@gmail.com>
2016-03-01 12:08:28 +0100

    add amount to transactions table

---
+++ database/migrations/2016_03_01_110720_update_transactions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateTransactionsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('transactions', function(Blueprint $table)
+		{
+			$table->string('amount');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('transactions', function(Blueprint $table)
+		{
+			$table->string('amount');
+		});
+	}
+
+}
