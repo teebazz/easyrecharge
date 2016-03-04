5d4880202f66230aceeccf27778a9033ec13abfc
Badejo Oluwatobi <teebazzy@gmail.com>
2016-03-04 20:54:24 +0100

    secon

---
+++ database/migrations/2016_03_04_195343_update_trans_table_2.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateTransTable2 extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('transactions', function(Blueprint $table)
+		{
+			$table->string("phone",30);
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('transactions', function(Blueprint $table)
+		{
+			$table->string("phone",30);
+		});
+	}
+
+}
